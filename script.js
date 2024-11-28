4635966e7793eb65c842f6021527455cf78b83e5
E-C-Shackelford <ecs.shackelford@gmail.com>
2024-11-27 17:24:29 -0800

    feat: refine responsiveness and implement additional interactive features
    Adjusted hero-carousel section media queries to improve responsiveness, updated position-responsive mixin with additional query-type interpolation and changed the media query width value and applied these changes to selectors, added a bottom margin to the about section and a bottom border for tablet screens, refactored footer layout by adding a width to its text container and wrapped the sentences within the paragraph in spans for layout adjustments across screen sizes, implemented navigation behavior on screen resize to prevent glitches and implemented image and text updates in the hero section based on navigation interaction

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const menuToggle = document.querySelector(".c-nav__menu-toggle");
 const closeToggle = document.querySelector(".c-nav__close-toggle");
 const navList = document.querySelector(".c-nav__list");
 const overlay = document.querySelector(".c-overlay");
+const mobileCutOffBreakpoint = 820;
 
-//// Nav Scroll Effect
+////
+// Nav Scroll Effect
+////
 
 // track scrolling to toggle 'scrolled' class
 window.addEventListener("scroll", () => {
@@ -17,7 +20,9 @@ window.addEventListener("scroll", () => {
   }
 });
 
-//// Mobile Menu Toggle/Close Toggle and Sliding Nav Panel
+////
+// Mobile Menu Toggle/Close Toggle and Sliding Nav Panel
+////
 
 // the parameter determines whether the navigation panel is open
 function toggleNavPanel(isOpen) {
@@ -50,3 +55,83 @@ function toggleNavPanel(isOpen) {
 menuToggle.addEventListener("click", () => toggleNavPanel(true));
 // closing navigation panel
 closeToggle.addEventListener("click", () => toggleNavPanel(false));
+
+////
+// Nav Behavior with Screen Resize
+////
+
+// avoid glitches when switching between mobile, tablet, and desktop views
+window.addEventListener("resize", function () {
+  // retrieve the current width of the browser viewport
+  // does the current viewport match the desktop size?
+  const isDesktop = this.window.innerWidth >= mobileCutOffBreakpoint;
+  if (isDesktop) {
+    toggleNavPanel(false);
+    // communicate the nav list is visible in desktop views
+    navList.setAttribute("aria-hidden", "false");
+    navList.classList.remove("active");
+    menuToggle.style.display = "none";
+    closeToggle.style.display = "none";
+    overlay.style.display = "none";
+    // handle mobile view
+  } else {
+    navList.setAttribute("aria-hidden", "true");
+    menuToggle.style.display = "block";
+  }
+});
+
+////
+// Hero-Carousel Nav Btns
+////
+
+// select all the slides
+const slides = document.querySelectorAll(".c-hero-carousel__slide");
+// track the index of the current active slide
+let currentIndex = 0;
+
+// index parameter represents the index of the slide that should become active
+function displaySlide(index) {
+  // loop through slides
+  // current slide and its current index
+  slides.forEach((slide, i) => {
+    // is the current slide's index the same as the passed index?
+    // add '--active' class if true, remove if false
+    slide.classList.toggle("c-hero-carousel__slide--active", i === index);
+  });
+}
+
+// loop through each slide and only select its specific navigation elements
+slides.forEach((slide) => {
+  const navContainers = slide.querySelectorAll(
+    ".c-hero-carousel__nav--image, .c-hero-carousel__nav--cta"
+  );
+
+  // in the current slide, loop through each nav container and select its specific buttons
+  navContainers.forEach((nav) => {
+    const nextBtn = nav.querySelector(".c-hero-carousel__nav-btn--next");
+    const prevBtn = nav.querySelector(".c-hero-carousel__nav-btn--previous");
+
+    // ensure proper looping when navigating through the next new slides
+    if (nextBtn) {
+      // move current index forward by 1 slide and our result stays within the valid range of slide indices
+      nextBtn.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        displaySlide(currentIndex);
+      });
+    }
+
+    // ensure proper looping when navigating through previous slides
+    if (prevBtn) {
+      prevBtn.addEventListener("click", () => {
+        // move current index back by 1 slide and add the total number of slides, so even if it results in a negative index,
+        // it still loops correctly to the previous slide, and our result stays within the valid range of slide indices
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        displaySlide(currentIndex);
+      });
+    }
+  });
+});
+
+// initial call displays the first slide as active on page load
+// after each navigation interaction, the function is called to use currentIndex to update the DOM and display the current active slide
+displaySlide(currentIndex);
